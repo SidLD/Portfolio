519c67c72e433ec52eef10c1e56285f67430c635
Sid <cidlibril5@gmail.com>
2023-06-16 19:36:25 +0800

    final

--- src/layout/RootLayout.js
+++ src/layout/RootLayout.js
@@ -3,9 +3,10 @@ import { NavLink, Outlet, Link } from 'react-router-dom'
 
 // import logo from "../asset/icons8-portfolio-48.png"
 function RootLayout() {
-  let slideIndex = 0;
   const slide = (e) => {
-    console.log("s")
+    console.log(e)
+  //  const item =  document.getElementById('p')
+  //  console.log(item)
   }
   return (
     <div className="root-layout">
@@ -14,9 +15,9 @@ function RootLayout() {
               <Link to='/' className='logo-text'>Portfolio</Link>
           </div>
             <nav>
-                <NavLink className="link" onClick={slide()} to="">About</NavLink>
-                <NavLink className="link" onClick={slide()} to="project">Projects</NavLink>
-                <NavLink className="link" onClick={slide()} to="contact">Contact</NavLink>
+                <NavLink className="link" id='h' onClick={slide()} to="">About</NavLink>
+                <NavLink className="link" id='p'onClick={slide()} to="project">Projects</NavLink>
+                <NavLink className="link" id='c'onClick={slide()} to="contact">Contact</NavLink>
             </nav>
        </header>
        <main>
